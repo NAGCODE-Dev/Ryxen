04cf67074130e444bf5271dfd927abaa7002beb9
NAGCODE-Dev <nagcode.contact@gmail.com>
2026-04-08 18:50:48 -0300

    configure athlete render surfaces

---
+++ apps/athlete/services/renderSurfaceConfig.js
@@ -0,0 +1,31 @@
+export const ATHLETE_RENDER_SURFACES = [
+  {
+    refKey: 'headerAccount',
+    signatureKey: 'headerSignature',
+    htmlKey: 'headerHtml',
+    buildSignatureKey: 'buildHeaderSignature',
+    renderContentKey: 'renderHeaderAccount',
+  },
+  {
+    refKey: 'main',
+    signatureKey: 'mainSignature',
+    htmlKey: 'mainHtml',
+    buildSignatureKey: 'buildMainSignature',
+    renderContentKey: 'renderMainContent',
+  },
+  {
+    refKey: 'bottomNav',
+    signatureKey: 'bottomSignature',
+    htmlKey: 'bottomHtml',
+    buildSignatureKey: 'buildBottomSignature',
+    renderContentKey: 'renderBottomNav',
+  },
+  {
+    refKey: 'modals',
+    signatureKey: 'modalSignature',
+    htmlKey: 'modalHtml',
+    buildSignatureKey: 'buildModalSignature',
+    renderContentKey: 'renderModals',
+  },
+];
+
