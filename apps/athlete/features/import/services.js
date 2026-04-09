a60a01d93bf4c02c1bd4bac1fb1ae8109c445cd8
NAGCODE-Dev <nagcode.contact@gmail.com>
2026-04-09 00:49:56 -0300

    fix athlete import picker exports

--- apps/athlete/features/import/services.js
+++ apps/athlete/features/import/services.js
@@ -6,8 +6,10 @@ export {
   IMAGE_MAX_DIMENSION,
   IMAGE_TARGET_MAX_BYTES,
   IMPORT_HARD_MAX_BYTES,
+  prepareImportFileForClientUse,
+} from '../../services/importFiles.js';
+export {
   pickJsonFile,
   pickPdfFile,
   pickUniversalFile,
-  prepareImportFileForClientUse,
-} from '../../services/importFiles.js';
+} from '../../services/importPickers.js';
